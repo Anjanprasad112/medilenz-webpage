bf22301f9fd9fcc0eafb36586c6ae67bba3e7eed
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-13 11:31:07 +0530

    ftp image

--- src/components/homecomponents/sft/page.jsx
+++ src/components/homecomponents/sft/page.jsx
@@ -6,7 +6,7 @@ const SecureFile = () => {
     <div className={`${styles.Container} container-fluid`}>
         <div className={`row align-items-center justify-content-center`}>
             <div className={`col-md-6 ${styles.ImageContainer}`}>
-                <Image src="/FTP.jpg" alt="ftp" width={300} height={300}/>
+                <Image src="/FTP.jpg" alt="ftp" width={400} height={400}/>
             </div>
             <div className={`col-md-6 ${styles.TextContainer}`}>
                 <h2>Secure File Transfer</h2>
