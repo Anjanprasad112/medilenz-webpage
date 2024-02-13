69437d2370e080e88dac2ce2a088a7138079e4fa
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-13 19:50:00 +0530

    text content updated

--- src/components/homecomponents/wmud/page.jsx
+++ src/components/homecomponents/wmud/page.jsx
@@ -8,7 +8,7 @@ const Diff = () => {
       <div className={`row justify-content-center ${styles.Container}`}>
         <div className={`col-md-4 col-sm-6 ${styles.BoxContainer}`}>
           <div className={styles.LogoContainer}>
-            <FaPeopleGroup size={25} style={{ color: "#1872dd" }} />
+            <FaPeopleGroup size={25} style={{ color: "#0D6FED" }} />
           </div>
           <div className={styles.TextHeader}>
             Qualified and Experienced Medical Professionals
@@ -23,31 +23,33 @@ const Diff = () => {
 
         <div className={`col-md-4 col-sm-6 ${styles.BoxContainer}`}>
           <div className={styles.LogoContainer}>
-            <FaPeopleGroup size={25} style={{ color: "#1872dd" }} />
+            <FaPeopleGroup size={25} style={{ color: "#0D6FED" }} />
           </div>
           <div className={styles.TextHeader}>
-            Qualified and Experienced Medical Professionals
+          AI-Enabled proprietary software
           </div>
           <div className={styles.TextContainer}>
-            We have a team of highly qualified and 
-            experienced healthcare professionals 
-            who can review records of all
-            medical specialties.
+          It helps us manage the critical tasks
+of processing medical records and
+delivering Medical Indexes,
+Chronologies, Summaries, Mass-tort,
+and Expert Opinions.
           </div>
         </div>
 
         <div className={`col-md-4 col-sm-6 ${styles.BoxContainer}`}>
           <div className={styles.LogoContainer}>
-            <FaPeopleGroup size={25} style={{ color: "#1872dd" }} />
+            <FaPeopleGroup size={25} style={{ color: "#0D6FED" }} />
           </div>
           <div className={styles.TextHeader}>
-            Qualified and Experienced Medical Professionals
+          One-week delivery service
+for platinum partners
           </div>
           <div className={styles.TextContainer}>
-            We have a team of highly qualified and 
-            experienced healthcare professionals
-            who can review records of all
-            medical specialties.
+          With high-speed processing of both
+unstructured and structured data utilizing
+the latest AI techniques, we customize
+the services as per the client’s requirement.
           </div>
         </div>
       </div>
