eb3be02d760315a59f859ec4692ac22aa912cc86
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-18 08:50:39 +0530

    minor bugs fixed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,19 +1,19 @@
-import Core from "@/components/aboutcomponents/corecomp/page";
-import styles from './page.module.css';
-import Top from "@/components/aboutcomponents/topcomp/page";
-
+import { lazy, Suspense } from "react";
+const Core = lazy(() => import("@/components/aboutcomponents/corecomp/page"));
+import styles from "./page.module.css";
+const Top = lazy(() => import("@/components/aboutcomponents/topcomp/page"));
 
 const About = () => {
   return (
     <>
-    <div className={styles.Title}>
-      <div className={styles.Container}></div>
-      About Us
-    </div>
-    <Top/>
-    <Core/>
+      <div className={styles.Title}>
+        <div className={styles.Container}></div>
+        About Us
+      </div>
+      <Top />
+      <Core />
     </>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className={`nav-link active ${styles.customtext}`} aria-current="page" href="#">HOME</Link>
+          <Link className={`nav-link ${styles.customtext}`} aria-current="page" href="/">HOME</Link>
         </li>
        
         <li className="nav-item dropdown">
