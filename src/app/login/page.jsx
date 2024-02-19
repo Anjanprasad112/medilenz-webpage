7ac22b4acd8a5bf87467e506b20481db22463567
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-19 21:43:55 +0530

    err fixed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,9 +2,9 @@ import styles from './page.module.css';
 const Login = () => {
   return (
     <>
-      <div className={styles.Title}>
+     <div className={styles.Title}>
         <div className={styles.Container}></div>
-        Login
+       Login
       </div>
 
       <div className={styles.Main}>
@@ -14,18 +14,18 @@ const Login = () => {
           Seamless access to medical records through user-friendly login, ensuring healthcare professionals and patients experience a secure and intuitive platform for information retrieval.
         </div>
         <form className="d-flex flex-column align-items-center">
-          <div className="form-group w-50 m-1 mt-3">
+          <div className="form-group w-50 m-1">
             <input type="text" className="form-control" id="usernameOrEmail" placeholder="Username or Email" />
           </div>
           <div className="form-group w-50 m-1">
             <input type="password" className="form-control" id="password" placeholder="Password" />
           </div>
-          <div className="form-check-inline mb-3 m-1">
-            <input type="checkbox" className="form-check-input text-center px-2" id="robotCheck" />
-            <label className="form-check-label text-center px-2" htmlFor="robotCheck">I'm not a robot</label>
+          <div className="form-check-inline mb-3 w-75 m-1">
+            <input type="checkbox" className="form-check-input text-center" id="robotCheck" />
+            <label className="form-check-label text-center" htmlFor="robotCheck">I&apos;m not a robot</label>
           </div>
           <div className="w-75">
-            <p className="text-center">Don't have an account? <a href="/register">Register Now!</a></p>
+            <p className="text-center">Don&apos;t have an account? <a href="/register">Register Now!</a></p>
             <p className="text-center"><a href="/forgot-password">Forgot Password?</a></p>
           </div>
           <button type="submit" className="btn btn-primary w-30 m-2">Sign in</button>
