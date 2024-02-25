03adba4be32d6cc578f8769cae86715028787311
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-25 22:27:49 +0530

    350px width in service

--- src/components/services/page.jsx
+++ src/components/services/page.jsx
@@ -26,7 +26,7 @@ const Service = () => {
         <div key={card.id}>
           <div
             className="card flex justify-content-center align-items-center "
-            style={{ width: "325px",margin:"8px",borderRadius:"16px",boxShadow:"0 8px 30px 0 rgba(0, 0, 0, 0.1)" }}
+            style={{ width: "350px",margin:"8px",borderRadius:"16px",boxShadow:"0 8px 30px 0 rgba(0, 0, 0, 0.1)" }}
           >
             <Image
               src={card.imageSrc}
