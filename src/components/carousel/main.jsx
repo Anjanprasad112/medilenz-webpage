10862ac870639ed50f4e9f633040a69838215df5
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-26 11:49:31 +0530

    margin bottom updated

--- src/components/carousel/main.jsx
+++ src/components/carousel/main.jsx
@@ -3,7 +3,7 @@ import CarouselMain from './page'
 const main = () => {
   return (
     <div className='mt-5'>
-    <h3 className='text-center'>What our clients says about us</h3>
+    <h3 className='text-center mb-5'>What our clients says about us</h3>
     <CarouselMain data = {jsonData}/>
     {/* <Testimonials/> */}
     </div>
