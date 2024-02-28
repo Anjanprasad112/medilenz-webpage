abba57878f6ec4c999f0eec31c3ebb9e76a904db
Anjan Prasad <anjanprasad112@gmail.com>
2024-02-28 22:36:48 +0530

    services updated

--- src/components/services/page.jsx
+++ src/components/services/page.jsx
@@ -6,47 +6,70 @@ import cardsData from "./cards.json";
 import Link from "next/link";
 
 const Service = () => {
-
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
     setCards(cardsData);
   }, []);
   return (
-    
     <div className={styles.container}>
       <div className={styles.BG1}></div>
-        <div className={styles.BG2}></div>
+      <div className={styles.BG2}></div>
       <div>
-        <h3 className={styles.Text}>Our Services</h3>
+        <h2 className={styles.Text}>Our Services</h2>
       </div>
 
       <div
-        className={`d-flex flex-wrap justify-content-center gap-4 align-items-center ${styles.Box}`}
+        className={`d-flex flex-wrap justify-content-center align-items-center ${styles.Box}`}
       >
-        {cards.map((card)=>(
-        <Link key={card.id} href={`services/${card.id}`} className={styles.Hover}>
-          <div
-            className={`card flex justify-content-center align-items-center ${styles.CardS}`}
-            style={{margin:"10px 12px 10px 12px",borderRadius:"16px",boxShadow:"0 8px 30px 0 rgba(0, 0, 0, 0.1)" }}
+        {cards.map((card) => (
+          <Link
+            key={card.id}
+            href={`services/${card.id}`}
+            className={styles.Hover}
           >
-            <Image
-              src={card.imageSrc}
-              className="card-img-top"
-              alt="Service Image"
-              style={{borderRadius:"16px 16px 0px 0px"}}
-              width={300}
-              height={200}
-            />
-            <div className={`card-body ${styles.cardBody}`}>
-              <h5 className="card-title text-center text-black" style={{fontWeight:"bold",textDecoration:"none"}}>{card.title}</h5>
-              <p className="card-text text-center" style={{color:"#707070",lineHeight:"1.36",textDecoration:"none"}}>
-               {card.text}
-              </p>
+            <div
+              className={`card flex justify-content-center align-items-center ${styles.CardS}`}
+              style={{
+                margin: "10px 20px",
+                borderRadius: "16px",
+                boxShadow: "0 8px 30px 0 rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <Image
+                src={card.imageSrc}
+                className="card-img-top"
+                alt="Service Image"
+                style={{ borderRadius: "16px 16px 0px 0px" }}
+                width={300}
+                height={200}
+              />
+              <div className={`card-body ${styles.cardBody}`}>
+                <div
+                  className="card-title text-center text-black"
+                  style={{
+                    fontWeight: "600",
+                    textDecoration: "none",
+                    fontSize: "22px",
+                  }}
+                >
+                  {card.title}
+                </div>
+                <p
+                  className="card-text text-center"
+                  style={{
+                    color: "#707070",
+                    lineHeight: "1.36",
+                    textDecoration: "none",
+                    fontSize:"16px"
+                  }}
+                >
+                  {card.text}
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
       </div>
 
       <Link href={`/services`} className={styles.ButtonContainer}>
