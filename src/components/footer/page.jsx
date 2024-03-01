9612303351b2792bf7f0dc64f65c66f4ebfa5ca0
Anjan Prasad <anjanprasad112@gmail.com>
2024-03-01 16:14:19 +0530

    footer updated

--- src/components/footer/page.jsx
+++ src/components/footer/page.jsx
@@ -27,7 +27,8 @@ function Footer() {
   return (
     <>
       <SignUp />
-      <div className="container-md my-3">
+      <div className="container-xl
+       my-3">
         <footer
           className="text-center text-lg-start text-black"
           style={{ backgroundColor: "#fff" }}
@@ -36,60 +37,78 @@ function Footer() {
             <Row>
               <Col md={3} lg={3} xl={3} className="mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold">
-                We @ Medilenz
+                  We @ Medilenz
                 </h6>
                 <p className="fw-400">
-                Our team is comprised of doctors, each possessing
-more than a decade of clinical experience in various
-specialities. Our doctors are from India and abroad.
+                  Our team is comprised of doctors, each possessing more than a
+                  decade of clinical experience in various specialities. Our
+                  doctors are from India and abroad.
                 </p>
               </Col>
 
               <hr className="w-100 clearfix d-md-none" />
 
               <Col md={2} lg={2} xl={2} className="mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">
-                  Links
-                </h6>
+                <h6 className="text-uppercase mb-4 font-weight-bold">Links</h6>
                 <p>
-                  <Link href="#" className="text-black text-decoration-none fw-400">Blog</Link>
+                  <Link
+                    href="#"
+                    className="text-black text-decoration-none fw-400"
+                  >
+                    Blog
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#" className="text-black text-decoration-none fw-400">News&Gallery</Link>
+                  <Link
+                    href="#"
+                    className="text-black text-decoration-none fw-400"
+                  >
+                    News&Gallery
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#" className="text-black text-decoration-none fw-400">Became Patners</Link>
+                  <Link
+                    href="#"
+                    className="text-black text-decoration-none fw-400"
+                  >
+                    Became Patners
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#" className="text-black text-decoration-none fw-400">About Us</Link>
+                  <Link
+                    href="#"
+                    className="text-black text-decoration-none fw-400"
+                  >
+                    About Us
+                  </Link>
                 </p>
               </Col>
 
               <hr className="w-100 clearfix d-md-none" />
 
-              <Col md={4} lg={3} xl={3} className="mx-auto mt-3">
+              <Col md={4} lg={4} xl={4} className="mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold">
                   Contact
                 </h6>
                 <p>
-                  <TfiLocationPin style={{color:"var(--bg)",fontSize:"18px"}}/> <span className="fw-400">
-                  5 Harbor Point Drive, #302s Mill Valley, CA 94941
-                    </span>
+                  <TfiLocationPin
+                    style={{ color: "var(--bg)", fontSize: "18px" }}
+                  />{" "}
+                  <span className="fw-400">
+                    5 Harbor Point Drive, #302s Mill Valley, CA 94941
+                  </span>
                 </p>
                 <p>
-                  <FiMail style={{color:"var(--bg)",fontSize:"18px"}}/> <span className="fw-400">
-                  sales@medilenzlit.com
-                    </span>
+                  <FiMail style={{ color: "var(--bg)"}} />{" "}
+                  <span className="fw-400">sales@medilenzlit.com</span>
                 </p>
                 <p>
-                  <FiPhone style={{color:"var(--bg)",fontSize:"18px"}}/> <span className="fw-400">
-                  +1 (215) 219-1388
-                    </span> 
+                  <FiPhone style={{ color: "var(--bg)"}} />{" "}
+                  <span className="fw-400">+1 (215) 219-1388</span>
                 </p>
                 <p>
-                  <FiPhone style={{color:"var(--bg)",fontSize:"18px"}}/> <span className="fw-400">
-                  +1 (215) 391-1705
-                    </span>
+                  <FiPhone style={{ color: "var(--bg)"}} />{" "}
+                  <span className="fw-400">+1 (215) 391-1705</span>
                 </p>
               </Col>
 
@@ -102,35 +121,43 @@ specialities. Our doctors are from India and abroad.
                   // style={{ backgroundColor: "#3b5998" }}
                   href="#!"
                 >
-                  <FaFacebookF style={{color: "var(--bg)",fontSize:"16px"}}/>
+                  <FaFacebookF
+                    style={{ color: "var(--bg)" }}
+                  />
                 </Link>
                 <Link
                   className="btn btn-white btn-floating m-1"
                   // style={{ backgroundColor: "#55acee" }}
                   href="#!"
                 >
-                  <FaXTwitter style={{color: "var(--bg)",fontSize:"18px"}}/>
+                  <FaXTwitter
+                    style={{ color: "var(--bg)", }}
+                  />
                 </Link>
                 <Link
                   className="btn btn-white btn-floating m-1"
                   // style={{ backgroundColor: "#dd4b39" }}
                   href="#!"
                 >
-                  <FaYoutube style={{color: "var(--bg)",fontSize:"18px"}}/>
+                  <FaYoutube style={{ color: "var(--bg)" }} />
                 </Link>
                 <Link
                   className="btn btn-white btn-floating m-1"
                   // style={{ backgroundColor: "#ac2bac" }}
                   href="#!"
                 >
-                  <FaInstagram style={{color: "var(--bg)",fontSize:"18px"}}/>
+                  <FaInstagram
+                    style={{ color: "var(--bg)" }}
+                  />
                 </Link>
                 <Link
                   className="btn btn-white btn-floating m-1"
                   // style={{ backgroundColor: "#0082ca" }}
                   href="#"
                 >
-                  <FaLinkedinIn style={{color: "var(--bg)",fontSize:"18px"}}/>
+                  <FaLinkedinIn
+                    style={{ color: "var(--bg)" }}
+                  />
                 </Link>
                 {/* <a
                   className="btn btn-white btn-floating m-1"
@@ -145,16 +172,22 @@ specialities. Our doctors are from India and abroad.
 
           <hr className="w-80" />
 
-          <div className="text-center w-100" style={{ backgroundColor: '#fff' }}>
-          <div className={styles.BotmFooter}>
-            <p>© 2024 MEDILENZ. All Rights Reserved.</p>
-            <p>Developed by <Link href="https://vraio.in/" style={{ color:'var(--bg'}}>
-            Vraio Software Solutions Pvt Ltd
-              </Link></p>
-            <p>Privacy Policy</p>
-            <p>Terms & Conditions</p>
+          <div
+            className="text-center w-100"
+            style={{ backgroundColor: "#fff" }}
+          >
+            <div className={styles.BotmFooter}>
+              <p>© 2024 MEDILENZ. All Rights Reserved.</p>
+              <p>
+                Developed by{" "}
+                <Link href="https://vraio.in/" style={{ color: "var(--bg" }}>
+                  Vraio Software Solutions Pvt Ltd
+                </Link>
+              </p>
+              <p>Privacy Policy</p>
+              <p>Terms & Conditions</p>
+            </div>
           </div>
-        </div>
         </footer>
       </div>
     </>
